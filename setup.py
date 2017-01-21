ded5fcfd1cf99eff46cbb30508c4cc4f17c25cf9
Ruben Nijveld <ruben@gewooniets.nl>
2017-01-21 21:16:23 +0100

    Distribution files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dazeus',
+  packages = ['dazeus'],
+  version = '0.1',
+  description = 'Python bindings for DaZeus',
+  author = 'Aaron van Geffen, Ruben Nijveld',
+  author_email = 'aaron+pypi@aaronweb.net, ruben@gewooniets.nl',
+  url = 'https://github.com/dazeus/dazeus-python',
+  download_url = 'https://github.com/dazeus/dazeus-python/tarball/0.1',
+  keywords = ['dazeus', 'irc', 'bot'],
+  classifiers = [],
+)
